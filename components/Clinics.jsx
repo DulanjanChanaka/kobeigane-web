1fd4b3876d3d954208706ae30bbe8a2503d3f677
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-11-15 22:26:19 +0530

    test2

--- components/Clinics.jsx
+++ components/Clinics.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Headline from './Headline'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Clinics = () => {
 
@@ -19,10 +20,10 @@ const Clinics = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300' >
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <img src='https://m.media-amazon.com/images/I/61zs0PLlKrL.jpg' width='94' height='94' alt='/' className='rounded-lg'/>
+                        <Image src='/Assets/weight.png' width='94' height='94' alt='/' className='rounded-lg'/>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
-                        <h3>Bathroom Faucets</h3>
+                        <h3>Weight</h3>
                     </div>
                 </div>
                 </div> 
@@ -31,10 +32,10 @@ const Clinics = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <img src='https://m.media-amazon.com/images/I/71G7Ot4sbqL.jpg' width={94} height={94} alt='/' className='rounded-lg'/>
+                        <Image src='/Assets/hospital.png' width={94} height={94} alt='/' className='rounded-lg'/>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
-                        <h3>Bathroom Cabinets and Vanitiest</h3>
+                        <h3>Hospital</h3>
                     </div>
                 </div>
                 </div> 
@@ -44,10 +45,10 @@ const Clinics = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <img src='https://m.media-amazon.com/images/I/81-hXIwg4QL._AC_UF1000,1000_QL80_.jpg' width={94} height={94} alt='/' className='rounded-lg'/>
+                        <Image src='/Assets/mother.png' width={94} height={94} alt='/' className='rounded-lg'/>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
-                        <h3>Bathroom Mirrors</h3>
+                        <h3>Mother and Baby</h3>
                     </div>
                 </div>
                 </div> 
@@ -57,10 +58,10 @@ const Clinics = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <img src='https://m.media-amazon.com/images/I/71Ju+7017IL._AC_UF1000,1000_QL80_.jpg' width={94} height={94} alt='/' className='rounded-lg'/>
+                        <img src='/Assets/moh.png' width={94} height={94} alt='/' className='rounded-lg'/>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
-                        <h3>Bathroom Lighting</h3>
+                        <h3>MOH</h3>
                     </div>
                 </div>
                 </div> 
@@ -70,10 +71,10 @@ const Clinics = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <img src='https://i.pinimg.com/736x/a0/87/0b/a0870bf81ea287f1136bcbfbf139729b.jpg' width={94} height={94} alt='/' className='rounded-lg'/>
+                        <img src='/Assets/women.png' width={94} height={94} alt='/' className='rounded-lg'/>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
-                        <h3>Bathroom Tiles and Flooring</h3>
+                        <h3>Well Woman</h3>
                     </div>
                 </div>
                 </div> 
@@ -83,10 +84,10 @@ const Clinics = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <img src='https://m.media-amazon.com/images/I/6109KoUGZcS._AC_UF894,1000_QL80_.jpg' width={94} height={94} alt='/' className='rounded-lg'/>
+                        <img src='/Assets/thriposha.png' width={94} height={94} alt='/' className='rounded-lg'/>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
-                        <h3>Shelving and Towel Bars</h3>
+                        <h3>Threeposha</h3>
                     </div>
                 </div>
                 </div> 
@@ -96,92 +97,16 @@ const Clinics = () => {
                 <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
                 <div className='grid grid-cols-2 gap-4 justify-center items-center'>
                     <div className='m-auto'>
-                        <img src='https://www.showerdrape.co.uk/wp-content/uploads/2023/02/MODERNITY-CHROME-MAIN.jpg'width={94} height={94} alt='/' className='rounded-lg'/>
+                        <img src='/Assets/drugs.png'width={94} height={94} alt='/' className='rounded-lg'/>
                     </div>
                     <div className='flex flex-col items-center justify-center'>
-                        <h3>Bathroom Accessories</h3>
+                        <h3>Monthly</h3>
                     </div>
                 </div>
                 </div> 
                 </Link>
     
-                <Link href="/category/curtains">
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                    <div className='m-auto'>
-                        <img src='https://m.media-amazon.com/images/I/81F0YH3i0+L.jpg' width={94} height={94} alt='/' className='rounded-lg'/>
-                    </div>
-                    <div className='flex flex-col items-center justify-center'>
-                        <h3>Shower Curtains and Rods</h3>
-                    </div>
-                </div>
-                </div> 
-                </Link>
-    
-                <Link href="/category/hardware">
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                    <div className='m-auto'>
-                        <img src='https://m.media-amazon.com/images/I/61uD7CZ0q9L.jpg' width={94} height={94} alt='/' className='rounded-lg'/>
-                    </div>
-                    <div className='flex flex-col items-center justify-center'>
-                        <h3>Bathroom Hardware</h3>
-                    </div>
-                </div>
-                </div> 
-                </Link>
-
-                <Link href="/category/plumbing">
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                    <div className='m-auto'>
-                        <img src='https://w7.pngwing.com/pngs/809/971/png-transparent-traders-market-plumbing-fixtures-bathroom-building-materials-golden-pillars-miscellaneous-kitchen-building.png' width={94} height={94} alt='/' className='rounded-lg'/>
-                    </div>
-                    <div className='flex flex-col items-center justify-center'>
-                        <h3>Bathroom Plumbing Supplies</h3>
-                    </div>
-                </div>
-                </div> 
-                </Link>
-
-                <Link href="/category/cleaning">
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                    <div className='m-auto'>
-                        <img src='https://m.media-amazon.com/images/I/61tixhAwM6L.jpg' width={94} height={94} alt='/' className='rounded-lg'/>
-                    </div>
-                    <div className='flex flex-col items-center justify-center'>
-                        <h3>Bathroom Cleaning and Maintenance Products</h3>
-                    </div>
-                </div>
-                </div>
-                </Link> 
-
-                <Link href="/category/ventilation">
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                    <div className='m-auto'>
-                        <img src='https://cdn3.vectorstock.com/i/1000x1000/27/17/photorealistic-bathroom-ventilation-window-vector-1312717.jpg' width={94} height={94} alt='/' className='rounded-lg'/>
-                    </div>
-                    <div className='flex flex-col items-center justify-center'>
-                        <h3>Bathroom Ventilation</h3>
-                    </div>
-                </div>
-                </div>
-                </Link> 
-
-                <Link href="/category/heaters">
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                    <div className='m-auto'>
-                        <img src='https://i.pinimg.com/736x/af/9e/76/af9e760e0f38d91ca7ae9dae50f5a880--toilet-ideas-rain-shower.jpg' width={94} height={94} alt='/' className='rounded-lg'/>
-                    </div>
-                    <div className='flex flex-col items-center justify-center'>
-                        <h3>Water Heaters</h3>
-                    </div>
-                </div>
-                </div> 
-                </Link>
+               
     
                 </div>
             </div>

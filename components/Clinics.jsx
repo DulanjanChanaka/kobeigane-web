be646b7ec12a251220267009719b271d8fbe5c2b
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-11-15 22:53:16 +0530

    test4

--- components/Clinics.jsx
+++ components/Clinics.jsx
@@ -11,7 +11,7 @@ const Clinics = () => {
             <Headline title="Clinics"/>
                 
                 
-                <div className='grid grid-cols-2 lg:grid-cols-4 gap-8'>
+                <div className='grid lg:grid-cols-4 gap-8'>
     
     
      
